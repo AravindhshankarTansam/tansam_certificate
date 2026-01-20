267ea0b2c182b8ca93b124036ee9035decaa1baf
Kripali Singh <kripali@example.com>
2026-01-20 16:45:16 +0530

    admindash initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { RoleComponent } from './pages/role/role.component';
+import { LabComponent } from './pages/lab/lab.component';
+import { CreateUserComponent } from './pages/create-user/create-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'create-user', component: CreateUserComponent },
+  { path: 'role', component: RoleComponent },
+  { path: 'lab', component: LabComponent },
+  { path: '', redirectTo: 'role', pathMatch: 'full' }
+];
---
+++ src/app/pages/create-user/create-user.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-user',
+  standalone: true,
+  templateUrl: './create-user.component.html',
+  styleUrls: ['./create-user.component.css']
+})
+export class CreateUserComponent {}
