a433e9a9317728de66928171d21c70e8df5ca163
Monika <monika@tansam.org>
2026-01-29 14:30:22 +0530

    Done with the changes in the feature/tl_creation

---
+++ src/app/dashboard/team-lead/team-lead-dashboard/team-lead-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-lead-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './team-lead-dashboard.component.html',
+  styleUrl: './team-lead-dashboard.component.css'
+})
+export class TeamLeadDashboardComponent {
+
+}
